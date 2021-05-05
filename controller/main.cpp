159755b9cb6186fb455d67896d8e1b5f4ffe902a
tom.mansion <tomansion@yahoo.fr>
2021-05-05 17:19:24 +0200

    Implemented motor and photodiod code

--- controller/main.cpp
+++ controller/main.cpp
@@ -1,7 +1,60 @@
+#include "PinNames.h"
 #include "mbed.h"
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <type_traits>
+#include <utility>
+
+#include "../shared-lib/rpc_utilities.hpp"
+#include "../shared-lib/utilities.hpp"
+#include "../shared-lib/motor.hpp"
+// Initialize a pins to perform analog input and digital output fucntions
+
+/**
+ * Analysis, it returns pairs 
+ *
+ * @param socket Socket connected to PC
+ * @param start First step
+ * @param end Last step
+ * @param tick_rate Stride between steps
+ * @return Results
+ */
+std::vector<std::pair<float, float>> analyse(BufferedSerial& socket, float start, float end, float stride) {
+    AnalogIn   photodiodePin(p15);  
+
+    // Run analysis
+    std::vector<std::pair<float, float>> results;
+    auto step = start;
+    do {
+
+        // Goto step
+        chroma::motor_goto(step);
+
+        // Getting photodiode
+        // Pin p15
+        auto value = photodiodePin.read(); 
+        results.push_back(std::make_pair(step, value));
+
+        // Move to the next step
+        step += stride;
+
+    } while (step < end);
+
+    return results;
+}
+
 
 // main() runs in its own thread in the OS
 int main() {
-    while (true) {
-    }
+    std::srand(std::time(nullptr)); // use current time as seed for random generator
+    std::atomic<chroma::ControllerState> state(chroma::ControllerState::Booting);
+
+    // Run listening function
+    Thread listening_thread;
+    listening_thread.start([&]() { chroma::listen(state, analyse, chroma::calibrate); });
+
+    // Boot device and stay in idle state
+    chroma::to_idle(state);
+
 }
--- shared-lib/motor.hpp
+++ shared-lib/motor.hpp
@@ -1,10 +1,14 @@
 #pragma once
+#include "mbed.h"
 
+#include "PinNames.h"
+#include "mbed_wait_api.h"
 #include <cstddef>
+#include <cstdlib>
 #include <functional>
 
-#define _GRATING_REVOLUTION_STEP_COUNT 42 // TODO: TBD
-#define _MOTOR_WAVELENGTH_PER_STEP 1      // TODO: TBD
+#define _GRATING_REVOLUTION_STEP_COUNT 10000
+#define _MOTOR_WAVELENGTH_PER_STEP 0.3492
 
 #define _REFERENCE_STEP_INDEX 0
 #define _REFERENCE_WAVELENGTH_INDEX 1
@@ -25,7 +29,37 @@ static std::pair<uint32_t, float> _reference_step;
  * @param positive_way Rotation way
  */
 void rotate_grating(bool positive_way) {
-  // TODO: Implement it
+  // DigitalOut P1(p20), P2(p19), P3(p18), P4(p16);
+  DigitalOut P1(p7), P2(p8), P3(p9), P4(p10);
+
+  P1 = P2 = P3 = P4 = 0;
+ 
+  int end = positive_way ? 4 : 0;
+  int way = positive_way ? 1 : -1;
+
+  for (int i = positive_way ? 0 : 4; i != end; i+= way) {
+    switch (i) {
+    case 0:
+      P1 = 1; P2 = 0; P3 = 1; P4 = 0;
+      // led1 = 1; led2 = 0; led3 = 1; led4 = 0;
+      break;
+    case 1:
+      P1 = 0; P2 = 1; P3 = 1; P4 = 0;
+      // led1 = 0; led2 = 1; led3 = 1; led4 = 0;
+      break;
+    case 2:
+      P1 = 0; P2 = 1; P3 = 0; P4 = 1;
+      // led1 = 0; led2 = 1; led3 = 0; led4 = 1;
+      break;
+    case 3:
+      P1 = 1; P2 = 0; P3 = 0; P4 = 1;
+      // led1 = 1; led2 = 0; led3 = 0; led4 = 1;
+      break;
+    default:
+      break;
+    }
+    wait_us(100000);
+  }
 
   // Update current step
   _current_step = (_current_step + (positive_way ? 1 : -1)) %
@@ -43,6 +77,21 @@ float wavelength() {
              _MOTOR_WAVELENGTH_PER_STEP;
 }
 
+void motor_goto(float step) {
+
+  auto curLw = wavelength();
+
+  auto deltaToMove = step - curLw;
+
+  auto stepNumber = std::abs(deltaToMove) / _MOTOR_WAVELENGTH_PER_STEP;
+
+  auto way = deltaToMove > 0;
+
+  for (int i = 0; i < stepNumber; i++) {
+    rotate_grating(way);
+  }
+}
+
 /**
  * Calibrate grating motor with a known wavelength
  *
@@ -52,19 +101,27 @@ void calibrate(float wavelength) {
   // Consider current motor state as the step 0
   _current_step = 0;
 
+  // Loading photodiode in
+  AnalogIn   photodiodePin(p15);  
+
+  // Deteciton treshold
+  const float PHOTODIODE_REACTION_VALUE = 100;
+
   // Find step where the photodiode reacts
   auto index = -1;
   for (auto i = 0; i < _GRATING_REVOLUTION_STEP_COUNT; i++) {
-    // TODO: Break if photodiode reacts
-    // auto result = PHOTODIODE_VALUE;
-    // if (result > PHOTODIODE_REACTION_VALUE) {
-    //   index = i;
-    //   break;
-    // }
+    auto PHOTODIODE_VALUE =  photodiodePin.read(); 
+
+    // Break if photodiode reacts
+    auto result = PHOTODIODE_VALUE;
+    if (result > PHOTODIODE_REACTION_VALUE) {
+      index = i;
+      // break;
+    }
 
     // Move motor to the next step
     rotate_grating(true);
-  }
+}
 
   // TODO: Crash if index = -1
 
